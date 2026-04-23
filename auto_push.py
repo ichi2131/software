852581cc5931069b98e973f25bd5da9ef4d73c74
ichi2131 <shadowuwu32@gmail.com>
2026-04-22 19:43:11 -0500

    auto update

--- auto_push.py
+++ auto_push.py
@@ -1,15 +1,20 @@
 import os
 import time
 
+RUTA = r"C:\Users\shado\OneDrive\Desktop\chamba"
+
 while True:
+    os.chdir(RUTA)
+    
     status = os.popen("git status --porcelain").read()
     
     if status:
+        print("Cambios detectados...")
         os.system("git add .")
         os.system('git commit -m "auto update"')
         os.system("git push")
-        print("Subido 🚀")
+        print("Subido a GitHub 🚀")
     else:
         print("Sin cambios")
     
-    time.sleep(300)
+    time.sleep(300)  # cada 5 minutos
